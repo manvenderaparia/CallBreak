a1f771f379fc3750fbac559fa9faeadb55d6189d
manvenderaparia <112194223+manvenderaparia@users.noreply.github.com>
2023-11-08 00:46:29 -0500

    commiting

--- game.py
+++ game.py
@@ -1,4 +1,20 @@
 import pygame
 import sys
 pygame.init()
-screen= pygame.display.pygame.display.set_mode((800,400))
+#set Game resolution
+screen = pygame.display.pygame.display.set_mode((800,400))
+#Title of the game
+pygame.display.set_caption('Call Break')
+#Framerate of the Game
+clock=pygame.time.Clock()
+# Font inside the Game
+font = pygame.font.Font(None, 50)
+#calling picture in the Game
+Table_top=pygame.image.load('PNG/table.jpeg').convert()#convert_alpha for cards
+While True:
+for event in pygame.event.get():
+    if event.type() === pygame.QUIT:
+        pygame.quit()
+        exit()
+#now inserting the picture in the game 
+screen.blit(Table_top,(0,0))
